6e01168f57829e49f128099025cce7cb8f8aa34f
Robin Miller <robin@tenjin.ca>
2023-12-19 15:37:52 -0700

    Updated to FontAwesome 6.5.1

--- lib/fontawesome/free/version.rb
+++ lib/fontawesome/free/version.rb
@@ -3,6 +3,6 @@
 module FontAwesome
    module Free
       # First three should match the FA version, last version number is for patches to the gem wrapper
-      VERSION = '6.4.2.1'
+      VERSION = '6.5.1.0'
    end
 end
